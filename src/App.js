c4c66439d4c6c19cb621ea84717c49be974e49b4
Roman <romulus.hund@gmail.com>
2020-08-07 14:23:44 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, {useEffect} from 'react';
+import PageLayout from './components/PageLayout';
+import style from './css/App.scss';
+import {connect} from 'react-redux';
+import {fetchItems} from './store/actions/registerActions';
+
+const App = ({fetchItems}) => {
+
+    useEffect(() => {
+        fetchItems();
+    }, []);
+
+  return (
+      <div className={style['app-wrapper']}>
+        <PageLayout>
+          <div className="main-page-content">
+            Main
+          </div>
+        </PageLayout>
+      </div>
+  );
+};
+
+const mapStateToProps = state => {
+    return {
+        data: state.currData.data,
+    };
+};
+
+function mapDispatchToProps(dispatch) {
+    return {
+        fetchItems: () => dispatch(fetchItems())
+    };
+}
+
+export default  connect(mapStateToProps, mapDispatchToProps)(App);
